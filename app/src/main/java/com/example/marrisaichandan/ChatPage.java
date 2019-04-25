d18aadf4015f505d0e12bde610dea38596d3106c
Chandan <chandan3966.official@gmail.com>
2019-04-25 22:02:22 +0530

    first beta version

--- app/src/main/java/com/example/marrisaichandan/ChatPage.java
+++ app/src/main/java/com/example/marrisaichandan/ChatPage.java
@@ -19,6 +19,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sdsmdg.harjot.longshadows.LongShadowsImageView;
@@ -39,7 +40,9 @@ public class ChatPage extends AppCompatActivity {
     int select = 0;
     ViewPager viewPager;
     ConstraintLayout cl;
-
+    TextView rece,send;
+    int count = 0;
+    String[] common = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"};
 
 
     List<ChatBubble> chatBubbles;
@@ -49,10 +52,14 @@ public class ChatPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_page);
         rl = findViewById(R.id.layout);
+        rece = findViewById(R.id.receiver);
+        send = findViewById(R.id.sender);
+
 
 
         showdialogbox();
         listView = findViewById(R.id.listview);
+        listView.setSmoothScrollbarEnabled(true);
 
         cl = findViewById(R.id.constraintLayout);
 
@@ -102,29 +109,7 @@ public class ChatPage extends AppCompatActivity {
                     showdialogbox();
                 }
                 else {
-                    if(e.getText().toString().length()<1){
-                        Toast.makeText(getApplicationContext(),"list enter",Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        if (select == 0)
-                            select = 0;
-                        else if (select == 1)
-                            select = 1;
-                        else if (select == 2)
-                            select = 2;
-                        else if (select == 3)
-                            select = 3;
-                        else
-                            select = 4;
-                        ChatBubble Chatbubble = new ChatBubble(e.getText().toString(),select);
-                        chatBubbles.add(Chatbubble);
-                        adapter.notifyDataSetChanged();
-                        e.setText("");
-                        if (select == 4)
-                            select = 0;
-                        else
-                            select++;
-                    }
+                    loopchater();
                 }
             }
         });
@@ -137,30 +122,8 @@ public class ChatPage extends AppCompatActivity {
                     showdialogbox();
                 }
                 else {
-                    if(e.getText().toString().length()<1){
-                        Toast.makeText(getApplicationContext(),"layout enter",Toast.LENGTH_SHORT).show();
+                        loopchater();
                     }
-                    else {
-                        if (select == 0)
-                            select = 0;
-                        else if (select == 1)
-                            select = 1;
-                        else if (select == 2)
-                            select = 2;
-                        else if (select == 3)
-                            select = 3;
-                        else
-                            select = 4;
-                        ChatBubble Chatbubble = new ChatBubble(e.getText().toString(),select);
-                        chatBubbles.add(Chatbubble);
-                        adapter.notifyDataSetChanged();
-                        e.setText("");
-                        if (select == 4)
-                            select = 0;
-                        else
-                            select++;
-                    }
-                }
             }
         });
 
@@ -169,6 +132,27 @@ public class ChatPage extends AppCompatActivity {
     }
 
 
+    public void loopchater(){
+        if (select == 0)
+            select = 0;
+        else if (select == 1)
+            select = 1;
+        else if (select == 2)
+            select = 2;
+        else if (select == 3)
+            select = 3;
+        else
+            select = 4;
+        ChatBubble Chatbubble = new ChatBubble(common[count],select);
+        chatBubbles.add(Chatbubble);
+        adapter.notifyDataSetChanged();
+        e.setText("");
+        count++;
+        if (select == 4)
+            select = 0;
+        else
+            select++;
+    }
 
 
     public void imageretrive(){
@@ -176,24 +160,34 @@ public class ChatPage extends AppCompatActivity {
             if (img.equals("1")){
                 shadow.setImageResource(R.drawable.cat11);
                 rl.setBackgroundResource(R.drawable.emo1);
+                rece.setText("Sonia");
+                send.setText("Mum");
             }
             else if (img.equals("2")){
                 shadow.setImageResource(R.drawable.cat21);
                 rl.setBackgroundResource(R.drawable.fun);
+                rece.setText("Sunidhi");
+                send.setText("Aashi");
             }
             else if (img.equals("3")){
                 shadow.setImageResource(R.drawable.cat31);
                 rl.setBackgroundResource(R.drawable.anna);
+                rece.setText("Vijay");
+                send.setText("Doctor");
             }
             else{
                 shadow.setImageResource(R.drawable.cat41);
                 rl.setBackgroundResource(R.drawable.romance);
+                rece.setText("Sameer");
+                send.setText("Simran");
             }
         }
         else{
             if (img.equals("1")){
                 shadow.setImageResource(R.drawable.cat12);
                 rl.setBackgroundResource(R.drawable.emo2);
+                rece.setText("Mahira");
+                send.setText("Rehan");
             }
             else if (img.equals("2")){
 //            shadow.setImageResource(R.drawable.cat21);
